89fca1bbb5c8f615170ccf7810770f2969fd41f1
Arsalan Radhu <arsalan.radhu@dcmail.ca>
2021-12-01 12:54:24 -0500

    Added the SetWorkTicket()

--- src/ca/durhamcollege/ExtendedWorkTicket.java
+++ src/ca/durhamcollege/ExtendedWorkTicket.java
@@ -25,4 +25,21 @@ public class ExtendedWorkTicket extends WorkTicket
     {
         myOpen = false;
     }
+
+    // SetWorkTicket()
+    public boolean SetWorkTicket(int ticketNumber, String clientId, int year, int month, int day, String issueDescription,
+                                 boolean myOpen)
+    {
+        boolean isValid = true;
+
+        if (myOpen != true)
+        {
+            isValid = false;
+        }
+        else {
+            super.SetWorkTicket();
+            this.myOpen = myOpen;
+        }
+        return isValid;
+    }
 }
