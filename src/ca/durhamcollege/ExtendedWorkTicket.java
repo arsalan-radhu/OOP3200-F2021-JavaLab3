001721a23442ff6f9e72a60bb4bf1a4c6b9744c9
Arsalan Radhu <arsalan.radhu@dcmail.ca>
2021-12-01 12:28:53 -0500

    Added Mutators and Accessor to the ExtendedWorkTicket

--- src/ca/durhamcollege/ExtendedWorkTicket.java
+++ src/ca/durhamcollege/ExtendedWorkTicket.java
@@ -1,4 +1,28 @@
 package ca.durhamcollege;
 
-public class ExtendedWorkTicket {
+public class ExtendedWorkTicket extends WorkTicket
+{
+    //Declaration
+    boolean myOpen;
+
+    //ACCESSOR
+
+    //Returns open status
+    public boolean isOpen()
+    {
+        return myOpen;
+    }
+
+    //MUTATORS
+    // Opens Work Ticket
+    public void openTicket()
+    {
+        myOpen = true;
+    }
+
+    //Closes Work Ticket
+    public void closeTicket()
+    {
+        myOpen = false;
+    }
 }
